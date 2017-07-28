0fc378095b17a9228e589d9ae30a5e6349920223
Yong Cho Yin <me@techmastary.net>
2017-07-28 00:49:14 -0400

    SendActivity

---
+++ app/src/main/java/in/choy/messagessandbox/SendActivity.java
@@ -0,0 +1,13 @@
+package in.choy.messagessandbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SendActivity extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send);
+    }
+}
